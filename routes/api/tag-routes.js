70a5edd66fe01d1d077044ee3bfbc10a6b83c737
TheTrueGameBreaker <Davisgenthner@gmail.com>
2023-04-18 00:13:24 -0600

    Update 3

---
+++ routes/api/tag-routes.js
@@ -0,0 +1,28 @@
+const router = require('express').Router();
+const { Tag, Product, ProductTag } = require('../../models');
+
+// The `/api/tags` endpoint
+
+//get route
+router.get('/', (req, res) => {
+  // find all tags
+  // be sure to include its associated Product data
+  Tag.findAll({
+    attributes: ['id', 'tag_name'],
+    include: [
+      {
+        model: Product,
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+      }
+    ]
+  })
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+
+
+module.exports = router;
