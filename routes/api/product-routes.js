0fca2b84d524e242bf70144bc152c7d6d235b461
TheTrueGameBreaker <Davisgenthner@gmail.com>
2023-04-18 00:11:15 -0600

    New Push again

---
+++ routes/api/product-routes.js
@@ -0,0 +1,31 @@
+const router = require('express').Router();
+const { Product, Category, Tag, ProductTag } = require('../../models');
+
+// The `/api/products` endpoint
+
+// get all products
+router.get('/', (req, res) => {
+  // find all products
+  // be sure to include its associated Category and Tag data
+  Product.findAll({
+    attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+
+module.exports = router;
